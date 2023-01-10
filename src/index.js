4f722eabf0e2119b4da0ea3700a0a066f3c5cacc
gabicilfone <cilfonegabriel@gmail.com>
2023-01-10 13:51:13 -0300

    fix linter error

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@
 import './styles.css';
 
 // API to work: https://pokeapi.co/
-//Involvement API: https://www.notion.so/Involvement-API-869e60b5ad104603aa6db59e08150270
+// Involvement API: https://www.notion.so/Involvement-API-869e60b5ad104603aa6db59e08150270
