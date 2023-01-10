74f888cc8d4a1758995fcc241bcc3cc55e840e9b
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-10 15:46:45 -0600

    Display pokemons Items & Style HomePage

---
+++ src/pokemonDisplay.js
@@ -0,0 +1,26 @@
+import PokeData from '../modules/pokeData.js';
+
+async function PokemonDisplay() {
+  const api = new PokeData();
+  const pokemons = await api.getpokemons(1, 21);
+
+  const displayPokemon = document.getElementById('displayPokemon');
+  pokemons.forEach((pokemon) => {
+    displayPokemon.innerHTML += `
+        <div id="pokecards">
+        <p id="id">${pokemon.id}</p>
+        <div class="imgContainer">
+        <img id="image" src="${pokemon.image}" alt="">
+        <div class="shadow"></div>
+        </div>
+        <div class="footerImg">
+        <p id="types">${pokemon.pokemonTypes}</p>
+        <p id="name">${pokemon.name}</p>
+        </div>
+        <button id="btnDescription" type="submit"> Description </button>
+        <button id="btnReserve" type="submit"> reserve </button>
+        </div>
+        `;
+  });
+}
+export default PokemonDisplay;
