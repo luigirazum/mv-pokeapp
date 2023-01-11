a9b45cf7a26cd92a8224b84204e7b4c6426b01dc
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-11 15:09:22 -0600

    fix linters error

--- src/displayLikes.js
+++ src/displayLikes.js
@@ -11,32 +11,32 @@ const show = (data) => {
 };
 
 const likesGet = () => {
-    fetch(apiLikes, { method: 'GET' })
-      .then((res) => res.json())
-      .then((data) => {
-        show(data);
-      });
+  fetch(apiLikes, { method: 'GET' })
+    .then((res) => res.json())
+    .then((data) => {
+      show(data);
+    });
 };
 
 const likePost = (id) => {
-    fetch(apiLikes, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ item_id: id }),
-    });
-    const likeElement = document.querySelector(`#like${id}`);
-    const likesNum = parseInt(likeElement.innerHTML, 10);
-    likeElement.innerHTML = likesNum + 1;
+  fetch(apiLikes, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ item_id: id }),
+  });
+  const likeElement = document.querySelector(`#like${id}`);
+  const likesNum = parseInt(likeElement.innerHTML, 10);
+  likeElement.innerHTML = likesNum + 1;
 };
 
 const addLikes = () => {
-    const iconLike = document.querySelectorAll('.iconLike');
-    iconLike.forEach((like) => {
-      like.addEventListener('click', (e) => {
-        const { id } = e.target;
-        likePost(parseInt(id.substring(5), 10));
-      });
+  const iconLike = document.querySelectorAll('.iconLike');
+  iconLike.forEach((like) => {
+    like.addEventListener('click', (e) => {
+      const { id } = e.target;
+      likePost(parseInt(id.substring(5), 10));
     });
+  });
 };
-  
-  export { addLikes, likesGet };
+
+export { addLikes, likesGet };
