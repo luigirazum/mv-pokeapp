7387023e546a349b7d8e5d44540a324c1f6f6a50
Luis Zubia <soyzubia@gmail.com>
2023-01-11 15:36:23 -0600

    Add module involveapi for interactions.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // Import css styles for our project
 import './styles.css';
+import addLike from './modules/involapi.js';
 import PokemonDisplay from './pokemonDisplay.js';
 
 PokemonDisplay();
 
+const test = async () => addLike(1);
+
+console.log(addLike(1));
 // API to work: https://pokeapi.co/
 // Involvement API: https://www.notion.so/Involvement-API-869e60b5ad104603aa6db59e08150270
