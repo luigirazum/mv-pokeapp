3ec0e78867c6547117572e4d33549985ba2592c9
Luis Zubia <soyzubia@gmail.com>
2023-01-12 17:42:48 -0600

    Add list of reservations retrieved from the involvement API and inserted dinamically into the reservations popup card window.

--- src/modules/displayreserve.js
+++ src/modules/displayreserve.js
@@ -1,9 +1,10 @@
 import getPokemon, { getDescription } from './pokeapi.js';
+import { addReserve, getReserves } from './involapi.js';
 
 // Global reservePokemon object with pokemon details for the Reservations popup.
 
 const createPokemon = (values) => {
-  const attString = 'id, name, height, weight, exp, moves, image, description';
+  const attString = 'id, name, height, weight, exp, moves, image, description, reserves';
   const attributes = attString.replace(/\s/gi, '').split(',');
 
   const customPokemon = {};
@@ -18,6 +19,8 @@ const createPokemon = (values) => {
 const showReserve = async (id) => {
   const pokemonDetails = JSON.parse(await getPokemon(id));
   const pokemonDescription = JSON.parse(await getDescription(id));
+  const reserves = JSON.parse(await getReserves(id));
+
   const { descriptions } = pokemonDescription;
   const englishDescription = descriptions.find((description) => description.language.name === 'en');
   const { description } = englishDescription;
@@ -26,14 +29,63 @@ const showReserve = async (id) => {
   } = pokemonDetails;
   const { sprites: { other: { dream_world: { front_default: image } } } } = pokemonDetails;
 
-  const values = [Number(id), name, height, weight, exp, moves, image, description];
+  const values = [Number(id), name, height, weight, exp, moves, image, description, reserves];
 
   const reservePokemon = createPokemon(values);
 
   return reservePokemon;
 };
 
+const addFakeReserves = async (id) => {
+  const users = ['pokeBOT', 'pokeApp', 'pokeDEX'];
+  const startDates = ['2023-01-12', '2023-01-13', '2023-01-15'];
+  const endDates = ['2023-01-18', '2023-01-17', '2023-01-19'];
+
+  const res0 = {
+    item_id: id,
+    username: users[0],
+    date_start: startDates[0],
+    date_end: endDates[0],
+  };
+
+  const res1 = {
+    item_id: id,
+    username: users[1],
+    date_start: startDates[1],
+    date_end: endDates[1],
+  };
+
+  const res2 = {
+    item_id: id,
+    username: users[2],
+    date_start: startDates[2],
+    date_end: endDates[2],
+  };
+
+  await addReserve(res0).then(console.log);
+  await addReserve(res1).then(console.log);
+  await addReserve(res2).then(console.log);
+};
+
+const getReservesList = (reserves) => {
+  let allReserves = '';
+
+  reserves.forEach((reserve) => {
+    const reserveTemplate = `
+            <li class="popupres-reserveitem">
+              <span class="reserveitem-date">${reserve.date_start.replace('-', '/')}</span>
+              <span class="reserveitem-date">${reserve.date_end.replace('-', '/')}</span>
+              <span class="reserveitem-username">${reserve.username}</span>
+            </li>`;
+    allReserves += reserveTemplate;
+  });
+
+  return allReserves;
+};
+
 const getReserveTemplate = (pokemon) => {
+  const reserves = getReservesList(pokemon.reserves);
+
   const template = `
     <div class="popupres-window">
       <div class="popupres-header">
@@ -49,6 +101,12 @@ const getReserveTemplate = (pokemon) => {
         <p><b>Weight</b><span>${pokemon.weight}</span></p>
         <p><b>Base Experience</b><span>${pokemon.exp}</span></p>
         <p><b>Moves</b><span>${pokemon.moves}</span></p>
+      </div>
+      <div class="popupres-reserves">
+        <h3>Reservations (#)</h3>
+        <ul id="popupres-reservelist">
+          ${reserves}
+        </ul>
       </div>
     </div>`;
 
@@ -66,6 +124,8 @@ const displayReserve = (pokemon) => {
   div.innerHTML = pokemonPopupReserve;
 
   body.appendChild(div);
+
+  return pokemon;
 };
 
 const linkCloseReserveBtn = () => {
@@ -96,3 +156,4 @@ const linkReserveBtns = () => {
 };
 
 export { linkReserveBtns as default };
+export { addFakeReserves };
