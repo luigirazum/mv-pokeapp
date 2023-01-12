93cd8858f7182aaad3fa06b03785c9f4b3cc3035
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-11 19:03:10 -0600

    add assets,add file cards counter, add countercard

--- src/pokemonDisplay.js
+++ src/pokemonDisplay.js
@@ -1,6 +1,8 @@
 import PokeData from '../modules/pokeData.js';
 import addeventComment from './display-comment.js';
 import {addLikes, likesGet} from './displayLikes.js';
+import pokeCounter from './cartsCounter.js';
+
 
 async function PokemonDisplay() {
   const api = new PokeData();
@@ -11,17 +13,18 @@ async function PokemonDisplay() {
     displayPokemon.innerHTML += `
         <div class="pokecards">
         <p id="id">${pokemon.id}</p>
+        <div class="cir"></div>
+        <p id="name">${pokemon.name}</p>  
         <div class="imgContainer">
         <img id="image" src="${pokemon.image}" alt="">
         <div class="shadow"></div>
         </div>
+        <div class="footerImg">
+        <p id="types">${pokemon.pokemonTypes}</p>
         <p class="likes">
         <button class="iconLike" id="heart${pokemon.id}" alt="heart">&#10084;</button>
         <span><span class="likeTxt" id="like${pokemon.id}" > 0 </span> Likes </span>
         </p>
-        <div class="footerImg">
-        <p id="types">${pokemon.pokemonTypes}</p>
-        <p id="name">${pokemon.name}</p>
         </div>
         <button id="${pokemon.id}" class="btnDescription" type="submit"> Description </button>
         <button id="btnReserve" type="button"> reserve </button>
@@ -31,6 +34,7 @@ async function PokemonDisplay() {
   addeventComment();
   addLikes();
   likesGet();
+  pokeCounter();
 }
 
 export default PokemonDisplay;
