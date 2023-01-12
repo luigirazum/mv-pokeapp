0382ced9f2f7cb3232d84720935105e432b95b5f
gabicilfone <cilfonegabriel@gmail.com>
2023-01-12 18:24:52 -0300

    fix linters error

--- src/testjs/popup-counter.test.js
+++ src/testjs/popup-counter.test.js
@@ -5,42 +5,42 @@
 import counterCheck from '../popup-counter.js';
 
 const commentsListmock = (numComments) => {
-    document.body.innerHTML = '';
-    for (let i = 0; i < numComments; i += 1) {
-        const newComment = document.createElement('li');
-        newComment.innerHTML = ` 
+  document.body.innerHTML = '';
+  for (let i = 0; i < numComments; i += 1) {
+    const newComment = document.createElement('li');
+    newComment.innerHTML = ` 
         <p class="popup-comment-date">Hello</p>`;
-        newComment.classList = 'popup-comment-item';
-        document.body.appendChild(newComment);
-    }
+    newComment.classList = 'popup-comment-item';
+    document.body.appendChild(newComment);
+  }
 };
 
 describe('comments-counter', () => {
-    test('Correctly count the num of comments', () => {
-        const num = 0;
-        commentsListmock(num);
-        const count = counterCheck('popup-comment-item');
-        expect(count).toBe(0);
-    });
+  test('Correctly count the num of comments', () => {
+    const num = 0;
+    commentsListmock(num);
+    const count = counterCheck('popup-comment-item');
+    expect(count).toBe(0);
+  });
 
-    test('Check if class exist', () => {
-        const num = 10;
-        commentsListmock(num);
-        const count = counterCheck('clase-inexistente');
-        expect(count).toBe(0);
-    });
+  test('Check if class exist', () => {
+    const num = 10;
+    commentsListmock(num);
+    const count = counterCheck('clase-inexistente');
+    expect(count).toBe(0);
+  });
 
-    test('Correctly count', () => {
-        const num = 100;
-        commentsListmock(num);
-        const count = counterCheck('popup-comment-item');
-        expect(count).toBe(num);
-    });
+  test('Correctly count', () => {
+    const num = 100;
+    commentsListmock(num);
+    const count = counterCheck('popup-comment-item');
+    expect(count).toBe(num);
+  });
 
-    test('if the parameters of the class do not exist', () => {
-        const num = 2;
-        commentsListmock(num);
-        const count = counterCheck();
-        expect(count).toBe(0);
-    });
+  test('if the parameters of the class do not exist', () => {
+    const num = 2;
+    commentsListmock(num);
+    const count = counterCheck();
+    expect(count).toBe(0);
+  });
 });
